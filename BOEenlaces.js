c9e7c0af6fabd09134744a376f69478a365aea6d
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-10-14 00:28:32 +0200

    Update BOEenlaces.js

--- BOEenlaces.js
+++ BOEenlaces.js
@@ -1,5 +1,5 @@
 // Crear una variable que almacene la ruta del archivo JSON
-var ruta = "https://apdespanol.github.io/APdE.github.io/diccionario.json";
+var ruta = "diccionario.json";
 // Crear una variable que almacene el objeto diccionario vacío
 var diccionario = {};
 // Crear una función asíncrona que lea el archivo JSON y lo guarde en el diccionario
