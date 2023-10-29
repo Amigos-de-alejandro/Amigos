d27fc28ff97701c6dc4fcff0b419c8beeda1513d
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-10-29 16:45:44 +0100

    Update verificadordedocumentos.js

--- verificadordedocumentos.js
+++ verificadordedocumentos.js
@@ -1,19 +1,44 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pkijs/2.0.27/pkijs.js"></script>
+document.getElementById('uploadForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
     var formData = new FormData(this);
-    var documento = formData.get('documento');
+    var pdfFile = formData.get('documento');
 
-    var certificadoValido = verificarCertificado(documento);
+    // Leer el archivo PDF
+    var pdfData = await readFileAsync(pdfFile);
 
-    if (certificadoValido) {
-        alert('Certificado correcto. En principio, tenga la precaución de que este mecanismo no es 100% certero. Le recomendamos verificación manual.');
+    // Decodificar el archivo PDF
+    var asn1 = pkijs.fromBER(pdfData);
+    var pdfContent = new Uint8Array(asn1.result.valueBlock.valueHex);
+
+    // Crear un objeto CMS SignedData
+    var cmsSignedData = new pkijs.SignedData({ schema: asn1.result });
+
+    // Verificar la firma
+    var verificationResult = await cmsSignedData.verify({
+        signer: 0, // Índice del firmante, puede variar si hay múltiples firmantes
+        trustedCerts: [], // Lista de certificados de confianza (opcional)
+        data: pdfContent // Contenido del PDF
+    });
+
+    if (verificationResult) {
+        alert('La firma digital es válida.');
     } else {
-        alert('Firma digital no válida. Ese documento no es íntegro ni seguro. No es nuestro. Borrelo y descárguelo de nuevo.');
+        alert('La firma digital no es válida.');
     }
 });
 
-function verificarCertificado(documento) {
-    var certificadoEsperado = "92837485dc17f35701a3d2cc587716ead3ad920def970c5bf953f0a5e9a0d1b5";
-    return documento === certificadoEsperado;
+function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+        var reader = new FileReader();
+
+        reader.onload = () => {
+            resolve(reader.result);
+        }
+
+        reader.onerror = reject;
+
+        reader.readAsArrayBuffer(file);
+    });
 }
