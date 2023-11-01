a46ac341c9f2e775061f0d11e18b584035ca627a
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-11-01 19:22:58 +0100

    Update verificadordedocumentos.js

--- verificadordedocumentos.js
+++ verificadordedocumentos.js
@@ -1,44 +1,59 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pkijs/2.0.27/pkijs.js"></script>
-document.getElementById('uploadForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+// Obtenemos el formulario, el campo de archivo, el botón y el div de resultado
+var formulario = document.getElementById("miFormulario");
+var archivo = document.getElementById("archivo");
+var boton = formulario.querySelector("button");
+var resultado = document.getElementById("resultado");
 
-    var formData = new FormData(this);
-    var pdfFile = formData.get('documento');
+// Definimos una matriz o lista con las direcciones de los archivos que queremos comparar
+var matriz = [
+  "https://example.com/documento1.pdf",
+  "https://example.com/documento2.pdf",
+  "https://example.com/documento3.pdf"
+];
 
-    // Leer el archivo PDF
-    var pdfData = await readFileAsync(pdfFile);
+// Añadimos un evento al botón para que al pulsarlo se ejecute la función comparar
+boton.addEventListener("click", comparar);
 
-    // Decodificar el archivo PDF
-    var asn1 = pkijs.fromBER(pdfData);
-    var pdfContent = new Uint8Array(asn1.result.valueBlock.valueHex);
-
-    // Crear un objeto CMS SignedData
-    var cmsSignedData = new pkijs.SignedData({ schema: asn1.result });
-
-    // Verificar la firma
-    var verificationResult = await cmsSignedData.verify({
-        signer: 0, // Índice del firmante, puede variar si hay múltiples firmantes
-        trustedCerts: [], // Lista de certificados de confianza (opcional)
-        data: pdfContent // Contenido del PDF
-    });
-
-    if (verificationResult) {
-        alert('La firma digital es válida.');
-    } else {
-        alert('La firma digital no es válida.');
-    }
-});
-
-function readFileAsync(file) {
-    return new Promise((resolve, reject) => {
-        var reader = new FileReader();
-
-        reader.onload = () => {
-            resolve(reader.result);
-        }
-
-        reader.onerror = reject;
-
-        reader.readAsArrayBuffer(file);
-    });
+// Definimos la función comparar, que recibe un evento como parámetro
+function comparar(evento) {
+  // Evitamos que el formulario se envíe al servidor
+  evento.preventDefault();
+  // Obtenemos el archivo seleccionado por el usuario
+  var archivoSeleccionado = archivo.files[0];
+  // Comprobamos si se ha seleccionado algún archivo
+  if (archivoSeleccionado) {
+    // Calculamos el hash del archivo seleccionado usando la librería CryptoJS
+    var reader = new FileReader();
+    reader.onload = function() {
+      var hash1 = CryptoJS.SHA256(CryptoJS.lib.WordArray.create(reader.result)).toString();
+      // Recorremos la matriz con un bucle for
+      for (var i = 0; i < matriz.length; i++) {
+        // Obtenemos la dirección del archivo de cada elemento
+        var direccion = matriz[i];
+        // Descargamos el archivo de la web usando la función fetch
+        fetch(direccion)
+          .then(function(respuesta) {
+            return respuesta.blob();
+          })
+          .then(function(blob) {
+            // Calculamos el hash del archivo descargado usando la librería CryptoJS
+            var reader2 = new FileReader();
+            reader2.onload = function() {
+              var hash2 = CryptoJS.SHA256(CryptoJS.lib.WordArray.create(reader2.result)).toString();
+              // Comparamos los hashes y mostramos el resultado en el div
+              if (hash1 == hash2) {
+                resultado.innerHTML += "<p>Los archivos son iguales</p>";
+              } else {
+                resultado.innerHTML += "<p>Los archivos son diferentes</p>";
+              }
+            };
+            reader2.readAsArrayBuffer(blob);
+          });
+      }
+    };
+    reader.readAsArrayBuffer(archivoSeleccionado);
+  } else {
+    // Mostramos un mensaje de error si no se ha seleccionado ningún archivo
+    resultado.innerHTML = "<p>No se ha seleccionado ningún archivo</p>";
+  }
 }
