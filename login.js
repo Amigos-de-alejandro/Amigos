5c935f96808a8f9b5288f811ac5ac29ab6aa7532
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-11-09 19:15:33 +0100

    Update login.js

--- login.js
+++ login.js
@@ -27,25 +27,39 @@ var submit = document.getElementById("submit");
 submit.addEventListener("click", function(event) {
     // Evitamos que el formulario se envíe por defecto
     event.preventDefault();
-    // Declaramos una variable para indicar si el login es correcto o no
-    var loginSuccess = false;
-    // Recorremos el array de objetos para validar el nombre de usuario y la contraseña
-    for (var i = 0; i < userData.length; i++) {
-        var validUsername = userData[i].username;
-        var validPassword = userData[i].password;
-        if (username.value === validUsername && password.value === validPassword) {
-            // Si el login es correcto, cambiamos el valor de la variable y salimos del bucle
-            loginSuccess = true;
-            break;
-        }
-    }
-    if (loginSuccess) {
-        // Si el login es correcto, cambiamos el valor de la variable loginStatus a "true" y lo guardamos en localStorage
-        localStorage.setItem("loginStatus", "true");
-        // Redirigimos al usuario a la página index2.htm
-        window.location.href = form.action;
-    } else {
-        // Si el login es incorrecto, mostramos un mensaje de error
-        alert("Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.");
-    }
+    // Usamos la función getCredential del objeto navigator para obtener las credenciales guardadas en el navegador
+    navigator.credentials.get({
+        password: true
+    })
+        .then(function(cred) {
+            // Comprobamos si hay credenciales guardadas o no
+            if (cred) {
+                // Si hay credenciales guardadas, las usamos como valores del nombre de usuario y la contraseña
+                username.value = cred.id;
+                password.value = cred.password;
+            }
+            // Declaramos una variable para indicar si el login es correcto o no
+            var loginSuccess = false;
+            // Recorremos el array de objetos para validar el nombre de usuario y la contraseña
+            for (var i = 0; i < userData.length; i++) {
+                var validUsername = userData[i].username;
+                var validPassword = userData[i].password;
+                if (username.value === validUsername && password.value === validPassword) {
+                    // Si el login es correcto, cambiamos el valor de la variable y salimos del bucle
+                    loginSuccess = true;
+                    break;
+                }
+            }
+            if (loginSuccess) {
+                // Si el login es correcto, redirigimos al usuario a la página index2.htm
+                window.location.href = form.action;
+            } else {
+                // Si el login es incorrecto, mostramos un mensaje de error
+                alert("Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.");
+            }
+        })
+        .catch(function(error) {
+            // Si hay algún error al obtener las credenciales, mostramos un mensaje de error
+            alert("Ha ocurrido un error al obtener las credenciales guardadas en el navegador. Inténtalo de nuevo.");
+        });
 });
