4120274881c90d58afd0647961f24c4dcefa1195
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-11-09 18:51:58 +0100

    user

--- login.js
+++ login.js
@@ -1,5 +1,21 @@
 // login.js
-// Este archivo contiene la lógica para validar el login del usuario
+// Este archivo contiene la lógica para validar el login del usuario y los datos de verificación de varios usuarios
+
+// Copiamos el contenido del archivo user.json y lo asignamos a una variable
+var userData = [
+    {
+        "username": "admin",
+        "password": "1234"
+    },
+    {
+        "username": "user1",
+        "password": "abcd"
+    },
+    {
+        "username": "user2",
+        "password": "efgh"
+    }
+];
 
 // Obtenemos los elementos del formulario
 var form = document.getElementById("login-form");
@@ -11,35 +27,23 @@ var submit = document.getElementById("submit");
 submit.addEventListener("click", function(event) {
     // Evitamos que el formulario se envíe por defecto
     event.preventDefault();
-    // Hacemos una petición al servidor para obtener el archivo json con los datos de verificación
-    fetch("https://amigos-de-alejandro.github.io/Amigos/9_JS/user.json")
-        .then(function(response) {
-            // Convertimos la respuesta en un objeto javascript
-            return response.json();
-        })
-        .then(function(data) {
-            // Declaramos una variable para indicar si el login es correcto o no
-            var loginSuccess = false;
-            // Recorremos el array de objetos para validar el nombre de usuario y la contraseña
-            for (var i = 0; i < data.length; i++) {
-                var validUsername = data[i].username;
-                var validPassword = data[i].password;
-                if (username.value === validUsername && password.value === validPassword) {
-                    // Si el login es correcto, cambiamos el valor de la variable y salimos del bucle
-                    loginSuccess = true;
-                    break;
-                }
-            }
-            if (loginSuccess) {
-                // Si el login es correcto, redirigimos al usuario a la página index2.htm
-                window.location.href = form.action;
-            } else {
-                // Si el login es incorrecto, mostramos un mensaje de error
-                alert("Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.");
-            }
-        })
-        .catch(function(error) {
-            // Si hay algún error en la petición, mostramos un mensaje de error
-            alert("Ha ocurrido un error al obtener los datos de verificación. Inténtalo de nuevo.");
-        });
+    // Declaramos una variable para indicar si el login es correcto o no
+    var loginSuccess = false;
+    // Recorremos el array de objetos para validar el nombre de usuario y la contraseña
+    for (var i = 0; i < userData.length; i++) {
+        var validUsername = userData[i].username;
+        var validPassword = userData[i].password;
+        if (username.value === validUsername && password.value === validPassword) {
+            // Si el login es correcto, cambiamos el valor de la variable y salimos del bucle
+            loginSuccess = true;
+            break;
+        }
+    }
+    if (loginSuccess) {
+        // Si el login es correcto, redirigimos al usuario a la página index2.htm
+        window.location.href = form.action;
+    } else {
+        // Si el login es incorrecto, mostramos un mensaje de error
+        alert("Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.");
+    }
 });
