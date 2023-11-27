2d6ad1fe7e70bfd50d6e79ce0cec38bcea198559
APdEspanol <131468509+APdEspanol@users.noreply.github.com>
2023-11-27 17:05:10 +0100

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -5,7 +5,7 @@ const CACHE = "pwabuilder-offline-page";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html"; // Change this name to your offline page
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,7 +16,10 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => {
+        cache.add(offlineFallbackPage); // Add the offline page to the cache
+        cache.addAll(["/", "/styles.css", "/icon.png"]); // Add other pages and resources to the cache
+      })
   );
 });
 
@@ -41,8 +44,14 @@ self.addEventListener('fetch', (event) => {
           return preloadResp;
         }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(event.request); // Check if there is a cached response for the request
+        if (cachedResp) {
+          return cachedResp; // Return the cached response if there is one
+        }
+        const networkResp = await fetch(event.request); // Otherwise, fetch from the network
+        cache.put(event.request, networkResp.clone()); // And save the network response in the cache for future requests
+        return networkResp; // Return the network response
       } catch (error) {
 
         const cache = await caches.open(CACHE);
